fef2ca3261c55af282a89c5e143bd6ae831efa62
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2022-09-02 02:58:28 -0700

    controller scheduler

--- arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller.ino
@@ -1,6 +1,13 @@
 #include "BMC_SBUS.h"
 #include <arduino-timer.h>
 
+
+enum Direction {
+  FORWARD,
+  REVERSE
+} Motor;
+
+
 // channels
 #define PAN_CH 1
 #define TILT_CH 2
@@ -11,54 +18,40 @@
 BMC_SBUS mySBUS;
 
 const int sbusWAIT = 7; //frame timing delay in msecs
-const int sbusSIGNAL = 110;
+const int reverseSPEED = 2047-400;
+const int forwardSPEED = 500;
+int sbusSIGNAL = forwardSPEED;
 
-/*
- * @brief convert sbus packet values to something more readable as an argument
- * @param speed value can be from -10 to 10
- * @return sbus packet value
-*/
-//int speed_controller(int speed_value) {
-//  if (abs(speed_value) > 10) return 0;
-//  else return (speed_value, -10, 10, 0, 2047);
-//}
-//TODO: implement
+Timer<2> scheduler;
 
-Timer<1> timer;
 
-int panValue = 0;
+void stop_action(void) {
+  sbusSIGNAL = STOP;
+  
+}
 
-void motor_routine(void) {
+void reverse(void) {
+  sbusSIGNAL = reverseSPEED;
+}
+
+void motor_controller(void) {
     // Set sbus tilt
-    mySBUS.Servo(PAN_CH, panValue);
+    mySBUS.Servo(PAN_CH, sbusSIGNAL);
    
     // Update SBUS object and send data
     mySBUS.Update();
     mySBUS.Send();
+    
 }
 
-void motor_routine_back(void) {
-    // Set sbus tilt
-    mySBUS.Servo(PAN_CH, 2047-panValue);
-    // Update SBUS object and send data
-    mySBUS.Update();
-    mySBUS.Send();
-}
 void setup() {
   // Start BMC_SBUS object
+  
   mySBUS.begin();
+  scheduler.every(sbusWAIT, motor_controller);
+  scheduler.at(7000, stop_action);
 }
 
-bool dir = false;
-
 void loop() {
-  timer.tick();
-  
-  if (!dir && timer.ticks()%2000==0) {
-    motor_routine();
-    dir=!dir;
-  }if (dir && timer.ticks()%2000==0) {
-    motor_routine_back();
-    dir=!dir;
-  }
+  scheduler.tick();
 }
