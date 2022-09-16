52f711183c61343095ae6a60041d8b0aa860a39f
3LucasZ <lucas.j.zheng@gmail.com>
2022-09-16 16:37:32 -0700

    arduino gimbal triple channel proto

--- arduino_controller/arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller/arduino_controller.ino
@@ -2,30 +2,28 @@
 #include <arduino-timer.h>
 #include <SoftwareSerial.h>
 
-enum Direction {
-  FORWARD,
-  REVERSE
-};
-
 // channels
 #define PAN_CH 1
 #define TILT_CH 2
 #define ROLL_CH 4
+
+// sig
 #define STOP 1023 
+#define FW 500
+#define BW 1647
 
 // pins
 #define RX 3
 #define TX 4
 
+//time
+#define sbusWAIT 7
+
 //Declare BMC_SBUS Object
 BMC_SBUS mySBUS;
 SoftwareSerial rpi_receiver (RX, TX);
 
-
-const int sbusWAIT = 7; //frame timing delay in msecs
-const int reverseSPEED = 2047-400;
-const int forwardSPEED = 500;
-int sbusSIGNAL = STOP;
+int sbusSIGNAL[3];
 
 Timer<3> scheduler;
 
@@ -34,21 +32,27 @@ void read_serial(void) {
   if (rpi_receiver.available() > 0) {
     // read as char 
      int r = rpi_receiver.read()-'0';
-     Serial.println(r);
-     if (r == 0) sbusSIGNAL = STOP;
-     else if (r == 1) sbusSIGNAL = forwardSPEED;
-     else if (r == 2) sbusSIGNAL = reverseSPEED;
-     else if (r == 3) 
+     
+     if (r == 0) {
+      sbusSIGNAL[0] = STOP;
+      sbusSIGNAL[1] = STOP;
+      sbusSIGNAL[2] = STOP;
+     }
+     
+     else if (r == 1) sbusSIGNAL[0] = FW;
+     else if (r == 2) sbusSIGNAL[0] = BW;
+     else if (r == 3) sbusSIGNAL[1] = FW;
+     else if (r == 4) sbusSIGNAL[1] = BW;
+     else if (r == 5) sbusSIGNAL[2] = FW;
+     else if (r == 6) sbusSIGNAL[2] = BW;
   }
-}
-
-void stop_action(void) {
-  sbusSIGNAL = STOP;
 }
 
 void motor_controller(void) {
     // Set sbus tilt
-    mySBUS.Servo(PAN_CH, sbusSIGNAL);
+    mySBUS.Servo(PAN_CH, sbusSIGNAL[0]);
+    mySBUS.Servo(TILT_CH, sbusSIGNAL[1]);
+    mySBUS.Servo(ROLL_CH, sbusSIGNAL[2]);
 
     // Update SBUS object and send data
     mySBUS.Update();
