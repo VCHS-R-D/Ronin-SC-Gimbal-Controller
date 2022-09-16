0dd522ccb16032751d1471aca18492554df340c3
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2022-09-16 16:02:49 -0700

    gimbal works for pan

--- keyboard.py
+++ keyboard.py
@@ -6,8 +6,7 @@ ser = Serial("/dev/ttyAMA0", baudrate=9600)
 
 def on_press(key):
     try:
-        print(f'key {key.char} pressed ')
-        
+        print("pressed:", key.char)
         if key.char == '1':
             ser.write(b'1')
         if key.char == '2':
@@ -15,14 +14,8 @@ def on_press(key):
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
-
 def on_release(key):
-    print('{0} released'.format(
-        key))
-    if key == keyboard.Key.esc: # escape
-        # Stop listener
-        return False
-
+    ser.write(b'0');
 # ...or, in a non-blocking fashion:
 with keyboard.Listener(
         on_press=on_press,
